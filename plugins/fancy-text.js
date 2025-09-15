3cb1eadaeb43d125509ad9b36d34f27c9269dc48
BILAL TECH <penalacc786@gmail.com>
2025-09-16 00:16:15 +0500

    👑 BILAL-MD 👑

--- plugins/fancy-text.js
+++ plugins/fancy-text.js
@@ -4,29 +4,29 @@ const { cmd } = require("../command");
 cmd({
   pattern: "fancy",
   alias: ["font", "style"],
-  react: "✍️",
+  react: "🌹",
   desc: "Convert text into various fonts.",
   category: "tools",
   filename: __filename
 }, async (conn, m, store, { from, quoted, args, q, reply }) => {
   try {
     if (!q) {
-      return reply("❎ Please provide text to convert into fancy fonts.\n\n*Example:* .fancy chamod nimsara");
+      return reply("*AP NE KISI BHI TEXT KO STYLISH BANAN HAI TO ESE LIKHE ☺️❤️* \n *.FANCY BILAL-MD");
     }
 
     const apiUrl = `https://www.dark-yasiya-api.site/other/font?text=${encodeURIComponent(q)}`;
     const response = await axios.get(apiUrl);
     
     if (!response.data.status) {
-      return reply("❌ Error fetching fonts. Please try again later.");
+      return reply("*DUBARA KOSHISH KAREIN 🥺❤️*");
     }
 
     const fonts = response.data.result.map(item => `*${item.name}:*\n${item.result}`).join("\n\n");
-    const resultText = `✨ *Fancy Fonts Converter* ✨\n\n${fonts}\n\n> *Powered by BILAL-MD*`;
+    const resultText = `*🌹 FANCY TEXTS 🌹* \n\n${fonts}\n\n *👑 BILAL-MD WHATSAPP BOT 👑**`;
 
     await conn.sendMessage(from, { text: resultText }, { quoted: m });
   } catch (error) {
-    console.error("❌ Error in fancy command:", error);
-    reply("⚠️ An error occurred while fetching fonts.");
+    console.error("*DUBARA KOSHISH KAREIN 🥺❤️*", error);
+    reply("*DUBARA KOSHISH KAREIN 🥺❤️*");
   }
 });
